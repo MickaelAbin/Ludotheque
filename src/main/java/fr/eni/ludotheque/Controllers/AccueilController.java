d1355363d9d00b080262e4ffe01d79538a84dc50
makel44 <120366866+makel44@users.noreply.github.com>
2024-12-10 15:37:55 +0100

    autorisation routes ok bouttons affichés en fonction des roles ok

--- src/main/java/fr/eni/ludotheque/Controllers/AccueilController.java
+++ src/main/java/fr/eni/ludotheque/Controllers/AccueilController.java
@@ -2,21 +2,26 @@ package fr.eni.ludotheque.Controllers;
 
 import fr.eni.ludotheque.bll.ClientService;
 import fr.eni.ludotheque.bo.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 @Controller
 public class AccueilController {
 
     private ClientService clientService;
-
-    public AccueilController(ClientService clientService) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    public AccueilController(ClientService clientService,PasswordEncoder passwordEncoder) {
         super();
         this.clientService = clientService;
     }
+
 
     @ModelAttribute("client")
     public Client createClient() {
@@ -34,4 +39,10 @@ public class AccueilController {
     public String accueil(Model model) {
         return "accueil";
         }
+    @GetMapping("/chiffrer")
+    public String chiffrer(@RequestParam("mot") String mot) {
+        System.out.println("[" + passwordEncoder.encode(mot) + "]");
+        return "redirect:/";
+    }
 }
+
--- src/main/java/fr/eni/ludotheque/config/SpringSecurityConfig.java
+++ src/main/java/fr/eni/ludotheque/config/SpringSecurityConfig.java
@@ -22,19 +22,36 @@ import org.springframework.security.web.SecurityFilterChain;
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            return http.csrf().disable().authorizeHttpRequests(auth ->{
-                auth.requestMatchers("/jeu/creer").hasRole("ADMIN");
-                auth.requestMatchers("/jeu/{id}/edition").hasRole("ADMIN");
-                auth.requestMatchers("/jeu/{id}/suppression").hasRole("ADMIN");
+            return http.authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/jeux/creer").hasRole("ADMIN");
+                auth.requestMatchers("/jeux/{id}/edition").hasRole("ADMIN");
+                auth.requestMatchers("/jeux/{id}/suppression").hasRole("ADMIN");
                 auth.requestMatchers("/exemplaires/{id}/creer").hasRole("ADMIN");
                 auth.requestMatchers("/clients").hasAnyRole("EMPLOYE", "ADMIN");
-                auth.requestMatchers("/jeu/{id}/detail").hasAnyRole("EMPLOYE", "ADMIN", "UTILISATEUR");
+                auth.requestMatchers("/jeux/{id}/detail").hasAnyRole("EMPLOYE", "ADMIN", "UTILISATEUR");
                 auth.requestMatchers("/jeux").hasAnyRole("EMPLOYE", "ADMIN", "UTILISATEUR");
+                auth.requestMatchers("/chiffrer").hasAnyRole("EMPLOYE", "ADMIN", "UTILISATEUR");
                 auth.requestMatchers("/").hasAnyRole("EMPLOYE", "ADMIN", "UTILISATEUR");
                 auth.anyRequest().authenticated();
 
             }).formLogin(Customizer.withDefaults()).build();
         }
+        //        @Bean
+//        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//            return http.authorizeHttpRequests((requests)-> requests
+//                    .requestMatchers(HttpMethod.GET,"/jeu/creer").hasRole("ROLE_ADMIN")
+//                    .requestMatchers("/jeu/{id}/edition").hasRole("ROLE_ADMIN")
+//                    .requestMatchers("/jeu/{id}/suppression").hasRole("ROLE_ADMIN")
+//                    .requestMatchers("/exemplaires/{id}/creer").hasRole("ROLE_ADMIN")
+//                    .requestMatchers("/clients").hasAnyRole("ROLE_EMPLOYE", "ROLE_ADMIN")
+//                    .requestMatchers("/jeu/{id}/detail").hasAnyRole("ROLE_EMPLOYE", "ROLE_ADMIN", "ROLE_UTILISATEUR")
+//                    .requestMatchers("/jeux").hasAnyRole("ROLE_EMPLOYE", "ROLE_ADMIN", "ROLE_UTILISATEUR")
+//                    .requestMatchers("/").hasAnyRole("ROLE_EMPLOYE", "ROLE_ADMIN", "ROLE_UTILISATEUR")
+//                    .requestMatchers(HttpMethod.POST).hasAnyRole("ROLE_EMPLOYE", "ROLE_ADMIN")
+//                    .anyRequest().authenticated()
+//            )
+//                    .formLogin(Customizer.withDefaults()).build();
+//        }
 
         @Autowired
         private CustomUserDetailsService customUserDetailsService;
